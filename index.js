5600782fd5a848ea67ff05d4aba47e85dc6f472e
Dramoscastro <danielleramoscastro@gmail.com>
2020-06-25 15:12:58 -0400

    copying bootstrap portfolio

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 3000;
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// connecting back to front
+app.set("view engine", "ejs");
+
+//route home
+app.get("/", function (req, res) {
+  res.render("index.ejs");
+});
+
+//route portfolio
+app.get("/portfolio", function (req, res) {
+  res.render("index.ejs");
+});
+
+//route contact
+app.get("/contact", function (req, res) {
+  res.render("index.ejs");
+});
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
